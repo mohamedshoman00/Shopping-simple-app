fea93614d14ea09a7cc18de213507b4dbd417f1b
mohamedshoman00 <mohamedshoman242@gmail.com>
2023-03-01 15:32:15 +0200

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 const NavBar = (props) => {
+  // const [state, setState] = useState({ display: "", isActive: false });
   const styled = ({ isActive }) =>
     isActive ? { color: "white" } : { color: "#bdbdbd" };
 
+  const handleToggle = (e) => {
+    const btn = e.target.closest("button");
+    // if (state.display === "") {
+    //   const style = { display: "block" };
+    //   setState({ ...state, display: "block" });
+    // } else setState({ ...state, display: "" });
+  };
+  // console.log(state);
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark">
       <div className="container-fluid">
         <NavLink className="navbar-brand text-light" to="/">
           Shopping Chart
         </NavLink>
+        <button
+          id="btn-toggle"
+          onClick={handleToggle}
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse .text-light" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
@@ -44,7 +66,9 @@ const NavBar = (props) => {
             </li>
           </ul>
         </div>
-        <span className="badge bg-primary">{props.productsCount}</span>
+        <span id="counter1" className="badge bg-primary">
+          {props.productsCount}
+        </span>
       </div>
     </nav>
   );
